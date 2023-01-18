3d79628aefa509d6363105f95f18d2617147ae4c
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-01-17 22:19:30 -0300

    ADD entity Pessoa and Enderec + o

--- src/main/java/com/attornatus/avaliacao/entity/Endereco.java
+++ src/main/java/com/attornatus/avaliacao/entity/Endereco.java
@@ -1,23 +1,30 @@
 package com.attornatus.avaliacao.entity;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import jakarta.persistence.*;
+import lombok.*;
 
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Entity
 
 public class Endereco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
     private String logradouro;
+    @Column(nullable = false)
     private String cep;
+    @Column(nullable = false)
     private String numero;
+    @Column(nullable = false)
     private String cidade;
 
 }
