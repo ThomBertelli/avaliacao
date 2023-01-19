520d91007b6145127995c9d42dc9e8f8b312e390
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-01-19 18:39:45 -0300

    ADD listarPessoas service

--- src/main/java/com/attornatus/avaliacao/service/PessoaService.java
+++ src/main/java/com/attornatus/avaliacao/service/PessoaService.java
@@ -2,7 +2,9 @@ package com.attornatus.avaliacao.service;
 
 import com.attornatus.avaliacao.entity.Pessoa;
 import com.attornatus.avaliacao.entity.dto.PessoaRequestDTO;
+import com.attornatus.avaliacao.entity.dto.PessoaResponseDTO;
 import com.attornatus.avaliacao.exception.CadastroInvalidoException;
+import com.attornatus.avaliacao.exception.handlers.ResourceNotFoundException;
 import com.attornatus.avaliacao.repository.PessoaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,39 +22,33 @@ public class PessoaService {
     PessoaRepository repository;
     ObjectMapper mapper = new ObjectMapper();
 
-    public ResponseEntity salvar (PessoaRequestDTO pesssoaDTO) throws CadastroInvalidoException {
+    public ResponseEntity salvar (PessoaRequestDTO pesssoaRequestDTO) throws CadastroInvalidoException {
         Pessoa pessoaSalva = null;
         ObjectMapper mapper = new ObjectMapper();
-        Pessoa pessoa = mapper.convertValue(pesssoaDTO, Pessoa.class);
+        Pessoa pessoa = mapper.convertValue(pesssoaRequestDTO, Pessoa.class);
 
         try{
             pessoaSalva = repository.save(pessoa);
         }catch (Exception ex){
             throw new CadastroInvalidoException("Cadastro inválido");
         }
-        return new ResponseEntity(mapper.convertValue(pessoaSalva, PessoaRequestDTO.class), HttpStatus.OK);
-
-
-
-
-
-
-
+        return new ResponseEntity(mapper.convertValue(pessoaSalva, PessoaResponseDTO.class), HttpStatus.OK);
 
     }
 
 
-
-
-
-    public List<PessoaRequestDTO> listarPessoas(){
-        List<Pessoa> listPessoa = repository.findAll();
-        List<PessoaRequestDTO> listPessoaRequestDTO = new ArrayList<>();
-
-        for (Pessoa pessoa : listPessoa){
-            listPessoaRequestDTO.add(mapper.convertValue(pessoa, PessoaRequestDTO.class));
+    public ResponseEntity listarPessoas () throws ResourceNotFoundException {
+        List<Pessoa> pessoaList = repository.findAll();
+        if(pessoaList.isEmpty()){
+            throw  new ResourceNotFoundException("Nenhuma pessoa foi cadastrada.")
         }
-        return listPessoaRequestDTO;
+
+        List<PessoaResponseDTO> pessoaResponseDTOList = new ArrayList<>();
+
+        for (Pessoa pessoa : pessoaList){
+            pessoaResponseDTOList.add(mapper.convertValue(pessoa, PessoaResponseDTO.class));
+        }
+        return new ResponseEntity(pessoaResponseDTOList,HttpStatus.OK);
     }
 
 
